b2dd97d69be54fa188e022a28ec4ee8549c70b20
galyasteffano <36943653+galyasteffano@users.noreply.github.com>
2018-04-21 12:11:04 +0300

    Delete db_credentials.php

--- dessert_mall/private/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("DB_SERVER","localhost");    #delete before uploading to github
-    define("DB_USER","root");
-    define("DB_PASS","");
-    define("DB_NAME","dessert_mall");       #put the name of the db here
-
-?>
